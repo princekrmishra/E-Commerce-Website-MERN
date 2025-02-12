8f2fb0540fd77c8adeba9c38225b955bf9709a4f
Prince Mishra <kmishraprince@gmail.com>
2025-02-13 02:53:25 +0530

    search bar done

--- my-project/src/components/Header/index.jsx
+++ my-project/src/components/Header/index.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import Search from '../Search';
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import { FaCartShopping } from "react-icons/fa6";
+import { IoMdGitCompare } from "react-icons/io";
+import { FaRegHeart } from "react-icons/fa6";
+import Tooltip from '@mui/material/Tooltip';
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+      right: -3,
+      top: 13,
+      border: `2px solid ${theme.palette.background.paper}`,
+      padding: '0 4px',
+    },
+  }));
+  
 
 
 const Header = () => {
@@ -8,7 +25,7 @@ const Header = () => {
         <header>
             <div className='top-strip py-2 border-t-[1px] border-gray-250 border-b-[1px]'>
                 <div className='container'>
-                    <div className='flex item-centre justify-between'>
+                    <div className='flex items-center justify-between'>
 
                         <div className='col1 w-[50%]'>
                             <p className='text-[12px] font-[500]'>Get up to 15% off new season styles, limited time </p>
@@ -33,13 +50,54 @@ const Header = () => {
             </div>
 
             <div className='header py-3'>
-                <div className='container flex item-center justify-between'>
+                <div className='container flex items-center justify-between'>
                     <div className='col1 w-[25%] '>
                     <Link to={"/"}><img src="/logo.jpg"></img></Link>
                     </div>
-                    <div className='col2 w-[45%]'></div>
-                        <Search/>
-                    <div className='col3 w-[30%]'></div>
+                    <div className='col2 w-[45%]'>
+                    <Search/>
+                    </div>
+                        
+                    <div className='col3 w-[30%] flex items-center pl-5'>
+                        <ul className='flex items-center gap-3 w-full justify-end'>
+                            <li className='list-none'>
+                                <Link to="/login" className='link transition text-[15px] font-[500]'>Login</Link> |&nbsp;<Link to="/register" className='link transition text-[15px] font-[500]' >Register</Link> 
+                            </li>
+                            
+                            <li>
+                            <Tooltip title="Compare">
+                                <IconButton aria-label="cart">
+                                    <StyledBadge badgeContent={4} color="secondary">
+                                        <IoMdGitCompare />
+                                    </StyledBadge>
+                                </IconButton>
+                            </Tooltip>
+                            </li>
+
+                            <li>
+                            <Tooltip title="Wishlist">
+                                <IconButton aria-label="cart">
+                                    <StyledBadge badgeContent={4} color="secondary">
+                                        <FaRegHeart />
+                                    </StyledBadge>
+                                </IconButton>
+                            </Tooltip>
+
+                            </li>
+
+                            <li>
+                            <Tooltip title="My Cart">
+                                <IconButton aria-label="cart">
+                                    <StyledBadge badgeContent={4} color="secondary">
+                                        <FaCartShopping />
+                                    </StyledBadge>
+                                </IconButton>
+                            </Tooltip>
+                            
+                            </li>
+
+                        </ul>
+                    </div>
 
                 </div>
 
--- my-project/src/components/Search/index.jsx
+++ my-project/src/components/Search/index.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import '../Search/style.css'
+import React from 'react';
+import '../Search/style.css';
 import Button from '@mui/material/Button';
+import { ImSearch } from "react-icons/im";
 
- const Search = () => {
+const Search = () => {
     return (
         <div className='searchBox w-[100%] h-[50px] bg-[#e5e5e5] rounded-[15px] relative p-2'>
-            <input type='text' placeholder='Search for product...' className='w-full h-[35px] focus:outline-none bg-inherit p-2 text-[15px]'/>
-            <Button>Contained</Button>
-
+            <input 
+                type='text' 
+                placeholder='Search for product...' 
+                className='w-full h-[35px] focus:outline-none bg-inherit p-2 text-[15px]'
+            />
+            <button className='!absolute top-[7px] right-[5px] z-50 !w-[37px] !min-w-[37px] h-[37px] !rounded-full !text-black flex items-center justify-center'> 
+                <ImSearch className='text-[#4e4e4e]' />
+            </button>
         </div>
-    )
+    );
 }
 
 export default Search;
