24cd320cd20bd4236687c9a74455ba5ebec8f3b3
Prince Mishra <kmishraprince@gmail.com>
2025-02-16 23:42:09 +0530

    sub categories

--- my-project/src/components/BannerBox/index.jsx
+++ my-project/src/components/BannerBox/index.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
  const BannerBox = (props) => {
   return (
-    <div className="box bannerBox">
+    <div className="bannerBox flex justify-center items-center overflow-hidden rounded-xl shadow-lg py-8">
         <Link to='/'>
-            <img src={props.img} className='w-full'/>
+            <img src={props.img} className='w-full h-[250px] object-cover rounded-xl'/>
         </Link>
   </div>
   )
