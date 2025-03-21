fb936f91fea773b121867ed12fbde2f2464660ae
Prince Mishra <kmishraprince@gmail.com>
2025-03-22 03:32:57 +0530

    Page

--- my-project/src/components/Sidebar/index.jsx
+++ my-project/src/components/Sidebar/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import "../Sidebar/style.css"
@@ -7,13 +6,18 @@ import {Collapse} from 'react-collapse';
 import { FaAngleDown } from "react-icons/fa6";
 import { Button } from '@mui/material';
 import { FaAngleUp } from "react-icons/fa6";
+import RangeSlider from 'react-range-slider-input';
+import 'react-range-slider-input/dist/style.css';
+import Rating from '@mui/material/Rating';
 
 
  const Sidebar = () => {
     const [isOpenCategoryFilter, SetIsOpenCategoryFilter] = useState(true);
+    const [isOpenAvailFilter, SetisOpenAvailFilter] = useState(true);
+    const [isOpenSizeFilter, SetisOpenSizeFilter] = useState(true);
   return (
     <aside className='sidebar py-5'>
-        <div className="box">
+        <div className="box mt-3">
             <h3 className='w-full mb-3 text-[16px] font-[600] flex items-center pr-5'>Shop By Categories
                 <Button className='!w-[30px] !h-[30px] !min-w-[30px] !rounded-full !ml-auto !text-[#000]' onClick={() => SetIsOpenCategoryFilter(!isOpenCategoryFilter)}>
                     
@@ -31,44 +35,32 @@ import { FaAngleUp } from "react-icons/fa6";
             />
 
             <FormControlLabel control={<Checkbox size='small'/>} 
-                label="Fashion" 
+                label="Electronics" 
                 className='w-full' 
             />
             <FormControlLabel control={<Checkbox size='small'/>} 
-                label="Fashion" 
+                label="Bags" 
                 className='w-full' 
             />
             <FormControlLabel control={<Checkbox size='small'/>} 
-                label="Fashion" 
+                label="Footwear" 
                 className='w-full' 
             />
             <FormControlLabel control={<Checkbox size='small'/>} 
-                label="Fashion" 
+                label="Jewellary" 
                 className='w-full' 
             />
             <FormControlLabel control={<Checkbox size='small'/>} 
-                label="Fashion" 
+                label="Wellness" 
                 className='w-full' 
             />
             <FormControlLabel control={<Checkbox size='small'/>} 
-                label="Fashion" 
+                label="Beauty" 
                 className='w-full' 
             />
             <FormControlLabel control={<Checkbox size='small'/>} 
-                label="Fashion" 
+                label="Groceries" 
                 className='w-full' />
-            <FormControlLabel control={<Checkbox size='small'/>} 
-                label="Fashion" 
-                className='w-full' 
-            />
-            <FormControlLabel control={<Checkbox size='small'/>} 
-                label="Fashion" 
-                className='w-full' 
-            />
-            <FormControlLabel control={<Checkbox size='small'/>} 
-                label="Fashion" 
-                className='w-full' 
-            />
 
             </div>
             </Collapse>
@@ -76,34 +68,123 @@ import { FaAngleUp } from "react-icons/fa6";
 
 
 
-        <div className="box">
+        <div className="box mt-3">
             <h3 className='w-full mb-3 text-[16px] font-[600] flex items-center pr-5'>Availability
-                <Button className='!w-[30px] !h-[30px] !min-w-[30px] !rounded-full !ml-auto !text-[#000]' onClick={() => SetIsOpenCategoryFilter(!isOpenCategoryFilter)}>
+                <Button className='!w-[30px] !h-[30px] !min-w-[30px] !rounded-full !ml-auto !text-[#000]' onClick={() => SetisOpenAvailFilter(!isOpenAvailFilter)}>
                     
                     {
-                        isOpenCategoryFilter === true ? <FaAngleUp /> : <FaAngleDown />
+                        isOpenAvailFilter === true ? <FaAngleUp /> : <FaAngleDown />
                     }
                 
                 </Button>
             </h3>
-            <Collapse isOpened={isOpenCategoryFilter}>
+            <Collapse isOpened={isOpenAvailFilter}>
             <div className="scroll px-4 relative -left-[13px]">
             <FormControlLabel control={<Checkbox size='small'/>} 
-                label="Available" 
+                label="Available (17)" 
                 className='w-full' 
             />
             <FormControlLabel control={<Checkbox size='small'/>} 
-                label="In-Stock" 
+                label="In-Stock (10)" 
                 className='w-full' 
             />
             <FormControlLabel control={<Checkbox size='small'/>} 
-                label="Out-Of-Stock" 
+                label="Out-Of-Stock (17)" 
                 className='w-full' 
             />
 
             </div>
             </Collapse>
         </div>
+
+
+
+
+
+
+
+        <div className="box mt-3">
+            <h3 className='w-full mb-3 text-[16px] font-[600] flex items-center pr-5'>Size
+                <Button className='!w-[30px] !h-[30px] !min-w-[30px] !rounded-full !ml-auto !text-[#000]' onClick={() => SetisOpenSizeFilter(!isOpenSizeFilter)}>
+                    
+                    {
+                        isOpenSizeFilter === true ? <FaAngleUp /> : <FaAngleDown />
+                    }
+                
+                </Button>
+            </h3>
+            <Collapse isOpened={isOpenSizeFilter}>
+            <div className="scroll px-4 relative -left-[13px]">
+            <FormControlLabel control={<Checkbox size='small'/>} 
+                label="Small (17)" 
+                className='w-full' 
+            />
+            <FormControlLabel control={<Checkbox size='small'/>} 
+                label="Medium (10)" 
+                className='w-full' 
+            />
+            <FormControlLabel control={<Checkbox size='small'/>} 
+                label="Large (17)" 
+                className='w-full' 
+            />
+
+            <FormControlLabel control={<Checkbox size='small'/>} 
+                label="X-Large (17)" 
+                className='w-full' 
+            />
+
+            <FormControlLabel control={<Checkbox size='small'/>} 
+                label="XX-Large (17)" 
+                className='w-full' 
+            />
+
+
+            </div>
+            </Collapse>
+        </div>
+
+
+        <div className="box mt-4">
+            <h3 className='w-full mb-3 text-[16px] font-[600] flex items-center pr-5'>Filter By Price
+            </h3>
+            <RangeSlider />
+            <div className='flex pt-4 pb-2 priceRange'>
+                <span className='text-[13px]'>
+                    From: <strong className='text-dark'>Rs: {100}</strong>
+                </span>
+                <span className='ml-auto text-[13px]'>
+                    From: <strong className='text-dark'>Rs: {5000}</strong>
+                </span>
+
+            </div>
+        </div>
+
+        <div className="box mt-4">
+            <h3 className='w-full mb-3 text-[16px] font-[600] flex items-center pr-5'>Filter By Rating
+            </h3>
+            <div className="w-full">
+            <Rating name="size-small" defaultValue={5} size="small" readOnly w-full/>
+            </div>
+
+            <div className="w-full">
+            <Rating name="size-small" defaultValue={4} size="small" readOnly w-full/>
+            </div>
+
+            <div className="w-full">
+            <Rating name="size-small" defaultValue={3} size="small" readOnly w-full/>
+            </div>
+
+            <div className="w-full">
+            <Rating name="size-small" defaultValue={2} size="small" readOnly w-full/>
+            </div>
+
+            <div className="w-full">
+            <Rating name="size-small" defaultValue={1} size="small" readOnly w-full/>
+            </div>
+            
+        </div>
+
+        
     </aside>
   )
 }
--- my-project/src/pages/ProductListing/index.jsx
+++ my-project/src/pages/ProductListing/index.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from '../../components/Sidebar';
 import Typography from '@mui/material/Typography';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from '@mui/material/Link';
-
+import ProductItem from '../../components/ProductItem'
+import ProductItemListView from '../../components/ProductItemListView'
+import Button from '@mui/material/Button';
+import { IoGrid } from "react-icons/io5";
+import { LuMenu } from "react-icons/lu";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 const ProductListing = () => {
+  const [itemView, setIsItemView] = useState('grid');
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <section className='py-5'>
         <div className="container">
@@ -24,10 +39,111 @@ const ProductListing = () => {
       
       </Breadcrumbs>
         </div>
+
         <div className="bg-white p-2 mt-4">
         <div className='container flex gap-3'>
             <div className='sidebarWrapper w-[20%] h-full bg-white'>
                 <Sidebar />
+            </div>
+
+            <div className="rightContent w-[80%] py-3">
+
+              <div className='bg-[#f1f1f1] p-2 w-full mb-4 rounded-md flex-items-center justify-between'>
+                <div className="col1 flex items-center gap-3">
+                  <Button 
+                    className='!w-[40px] !h-[40px] !min-w-[40px] !rounded-full !text-[#000]'
+                    onClick={()=>setIsItemView('list')}
+                  >
+                    <LuMenu className='text-[rgba(0,0,0,0.7)]'/>
+                  </Button>
+
+                  <Button 
+                    className='!w-[40px] !h-[40px] !min-w-[40px] !rounded-full !text-[#000]'
+                    onClick={()=>setIsItemView('grid')}
+                  >
+                    <IoGrid className='text-[rgba(0,0,0,0.7)]'/>
+                  </Button>
+
+                <span className='text-[14px] font-[500] pl-3 text-[rgba(0,0,0,0.7)]'>There are 27 Products</span>
+
+                <div className="col2 ml-auto flex items-center justify-end gap-3 pr-4">
+                  <span className='text-[14px] font-[500] pl-3 text-[rgba(0,0,0,0.7)]'>
+                    Sort By
+                    </span>
+                    <Button
+                      id="basic-button"
+                      aria-controls={open ? 'basic-menu' : undefined}
+                      aria-haspopup="true"
+                      aria-expanded={open ? 'true' : undefined}
+                      onClick={handleClick}
+                      className='!bg-white !text-[12px] !text-[#000] !capitalize !border-2 border-[#000]'
+                    >
+                    Sales Highest to Low
+                  </Button>
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                          'aria-labelledby': 'basic-button',
+                        }}
+                      >
+                        <MenuItem onClick={handleClose} className='!text-[12px] !text-[#000] !capitalize'>Sales Highest to Low</MenuItem>
+                        <MenuItem onClick={handleClose} className='!text-[12px] !text-[#000] !capitalize'>Relevance</MenuItem>
+                        <MenuItem onClick={handleClose} className='!text-[12px] !text-[#000] !capitalize'>Name A to Z</MenuItem>
+                        <MenuItem onClick={handleClose} className='!text-[12px] !text-[#000] !capitalize'>Name Z to A</MenuItem>
+                        <MenuItem onClick={handleClose} className='!text-[12px] !text-[#000] !capitalize'>Price High to Low</MenuItem>
+                        <MenuItem onClick={handleClose} className='!text-[12px] !text-[#000] !capitalize'>Price Low to High</MenuItem>
+
+                      </Menu>
+                </div>
+
+                </div>
+
+                
+              </div>
+
+                  
+                <div className="grid grid-cols-4 md:grid-cols-4 gap-4">
+                {
+                    itemView === 'grid' ?
+                    <>
+                    <ProductItem />
+                  <ProductItem />
+                  <ProductItem />
+                  <ProductItem />
+                  <ProductItem />
+                  <ProductItem />
+                  <ProductItem />
+                  <ProductItem />
+                  <ProductItem />
+                  <ProductItem />
+                  <ProductItem />
+                  <ProductItem />
+                  </> 
+                  :
+
+                <>
+                <ProductItemListView/>
+                <ProductItemListView/>
+                <ProductItemListView/>
+                <ProductItemListView/>
+                <ProductItemListView/>
+                <ProductItemListView/>
+                <ProductItemListView/>
+                <ProductItemListView/>
+                <ProductItemListView/>
+                <ProductItemListView/>
+                <ProductItemListView/>
+                <ProductItemListView/>
+                
+                </>
+
+                }
+                  
+
+                </div>
             </div>
         </div>
         </div>
